b28fb8ca0179b1c6dee92bd3760eb5e4025e641f
NinoosMoshi <nalkaton@gmail.com>
2024-10-21 17:29:01 -0500

    upload project

---
+++ demo-monolith/src/main/java/com/ninos/DemoMonolithApplication.java
@@ -0,0 +1,13 @@
+package com.ninos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMonolithApplication.class, args);
+	}
+
+}
